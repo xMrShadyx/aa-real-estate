5efbe743b4065fb95fdef9e2c7aac5baf4264c95
H.Hyusein <60807697+xMrShadyx@users.noreply.github.com>
2021-12-01 18:37:46 +0200

    initial commit

---
+++ src/main/java/com/lastToCreateProject/aarealestate/AaRealEstateApplication.java
@@ -0,0 +1,13 @@
+package com.lastToCreateProject.aarealestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaRealEstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaRealEstateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lastToCreateProject/aarealestate/AaRealEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastToCreateProject.aarealestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaRealEstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
